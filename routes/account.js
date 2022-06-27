2957ccfcb5a4b2605b4017aad13e017a657c421c
DCXan <chen.david.1016@gmail.com>
2022-06-26 23:09:48 -0500

    styled log in and registration pages to match home

--- routes/account.js
+++ routes/account.js
@@ -8,12 +8,7 @@ const bcrypt = require("bcryptjs");
 // Display log in and register pages
 
 accountRouter.get("/login", (req, res) => {
-  let isRegistered = req.query.success;
-  if (isRegistered) {
-    res.render("login", { message: "Account created successfully." });
-  } else {
     res.render("login");
-  }
 });
 
 accountRouter.get("/register", (req, res) => {
@@ -23,8 +18,14 @@ accountRouter.get("/register", (req, res) => {
 // Allow user to register by submitting a desired username and password
 
 accountRouter.post("/register", async (req, res) => {
-  const username = req.body.username;
+  
+  const email = req.body.email;
   const password = req.body.password;
+
+  if (email.length == 0 || password.length == 0) {
+    return;
+  }
+
 
   // Check database to see if username already exists. If username exists return user to registration page and display error message
 
@@ -34,22 +35,22 @@ accountRouter.post("/register", async (req, res) => {
   //   defaults: {password: password}
   // })
 
-  const user = await models.User.findOne({
-    where: { username: username },
+  const account = await models.User.findOne({
+    where: { email: email },
   });
 
-  if (user != null) {
+  if (account != null) {
     res.render("register", {
-      message: "Username is taken. Please choose a different one.",
+      message: `The email address ${email} is already associated with an account.`,
     });
   } else {
     bcrypt.genSalt(10).then((salt) => {
       bcrypt.hash(password, salt).then((hash) => {
-        const user = models.User.build({
-          username: username,
+        const newAccount = models.User.build({
+          email: email,
           password: hash,
         });
-        user.save().then((savedUser) => {
+        newAccount.save().then((savedAccount) => {
           res.render("login", { message: "Account registration successful!" });
         });
       });
@@ -60,20 +61,20 @@ accountRouter.post("/register", async (req, res) => {
 // Allow user to log in with valid credentials in users DB.
 
 accountRouter.post("/login", async (req, res) => {
-  const username = req.body.username;
+  const email = req.body.email;
   const password = req.body.password;
 
   // Search users DB for an existing username/pw combo. If combo doesn't exist, return to login and display message.
 
-  if (username.length == 0 || password.length == 0) {
+  if (email.length == 0 || password.length == 0) {
     return;
   } else {
     const user = await models.User.findOne({
-      where: { username: username },
+      where: { email: email },
     });
 
     if (user == null) {
-      res.render("login", { message: "Invalid username and/or password." });
+      res.render("login", { message: "Invalid email and/or password." });
     } else {
       bcrypt
         .compare(password, user.password)
@@ -81,18 +82,18 @@ accountRouter.post("/login", async (req, res) => {
           if (passwordsEqual) {
             if (req.session) {
               req.session.userID = user.id;
-              req.session.username = user.username;
+              req.session.email = user.email;
             }
-            res.redirect("/posts");
+            res.redirect("/dashboard");
           } else {
             res.render("login", {
-              errorMessage: "Invalid username and/or password.",
+              errorMessage: "Invalid email and/or password.",
             });
           }
         })
         .catch((error) => {
           res.render("login", {
-            errorMessage: "Invalid username and/or password.",
+            errorMessage: "Invalid email and/or password.",
           });
         });
     }

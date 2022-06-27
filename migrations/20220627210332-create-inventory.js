56395390e12c3c08391263e2238cc7445d3245a4
DCXan <chen.david.1016@gmail.com>
2022-06-27 16:16:24 -0500

    updated inventory table

---
+++ migrations/20220627210332-create-inventory.js
@@ -0,0 +1,43 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Inventories', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      make: {
+        type: Sequelize.STRING
+      },
+      model: {
+        type: Sequelize.STRING
+      },
+      year: {
+        type: Sequelize.INTEGER
+      },
+      color: {
+        type: Sequelize.STRING
+      },
+      mileage: {
+        type: Sequelize.INTEGER
+      },
+      dealerID: {
+        type: Sequelize.INTEGER,
+        references: { model: 'Users', field: 'id'}
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Inventories');
+  }
+};

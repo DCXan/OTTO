a622b2574f1984a9b5bb11c95295abd31a75077b
unknown <calvin2nguyen@gmail.com>
2022-06-28 01:46:22 -0700

    car request

--- routes/buyer.js
+++ routes/buyer.js
@@ -1,6 +1,7 @@
 // Everything related to buyer dashboard
 
 const express = require("express")
+const { sequelize } = require("../models")
 const buyerRouter = express.Router()
 
 buyerRouter.get("/home", (req, res) => {
@@ -13,4 +14,25 @@ module.exports = buyerRouter
 
 buyerRouter.get("/request", (req, res) => {
   res.render("request")
+})
+
+buyerRouter.post("/request", (req, res) => {
+  const make = req.body.make
+  const model = req.body.model
+  const year = req.body.year
+  const color = req.body.color
+  const price = req.body.price
+  const mileage = req.body.mileage
+
+  const newRequest = models.carRequests.build({
+    make: make,
+    model: model,
+    year: year,
+    color: color,
+    maxPrice: price,
+    maxMileage: mileage,
+  })
+  newRequest.save().then(savedRequest => {
+    res.render("buyer")
+  })
 })

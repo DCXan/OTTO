cc02e199c33f2ec9c5f50b5272c44e1c9c18b6a1
DCXan <chen.david.1016@gmail.com>
2022-06-28 13:28:14 -0500

    modal window for adding car

--- routes/dealer.js
+++ routes/dealer.js
@@ -3,8 +3,15 @@
 const express = require("express")
 const dealerRouter = express.Router()
 
-dealerRouter.get("/dashboard", (req, res) => {
+dealerRouter.get("/dashboard", async (req, res) => {
+
+  // const myCars = await models.
+
   res.render("dealer-dashboard")
+})
+
+dealerRouter.get('/add-car', (req, res) => {
+  res.render('add-car')
 })
 
 module.exports = dealerRouter
